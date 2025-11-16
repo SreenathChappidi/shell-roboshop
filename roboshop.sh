585c5d0f03f027160237a5e23220cfb9655f458e
sreenath reddy <sreenath.chappidi89@gmail.com>
2025-11-16 16:03:58 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0caef3e699037dfa4"
+ZONE_ID="Z10216781V6L2CS6W61DJ"
+DOMAIN_NAME="riha.fun"
 
 for instance in $@
 do
@@ -9,9 +11,30 @@ do
     #Get Private IP
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" #mongodb.riha.fun
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" #riha.fun
     fi
 
     echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID\
+  --change-batch '
+  {
+    "Comment": "Updating record set"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$RECORD_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
+  '
 done
